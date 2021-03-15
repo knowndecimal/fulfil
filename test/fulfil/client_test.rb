80ff07dc276bbe3484bb5da9f2e65b16289faf31
Chris Moore <chris@knowndecimal.com>
2021-03-14 20:02:46 -0400

    Add tests/fixtures around client network methods
    This is the start of issue #8. We're mocking out the requests to
    Fulfil and returning fixture responses. These are golden path
    scenarios, but it would be great to get some error cases in here next.

--- test/fulfil/client_test.rb
+++ test/fulfil/client_test.rb
@@ -5,5 +5,34 @@ module Fulfil
     def test_invalid_client
       assert_raises('InvalidClientError') { Fulfil::Client.new(subdomain: nil, token: nil) }
     end
+
+    def test_find_one
+      stub_fulfil_get('sale.sale/213112', 'sale_sale')
+
+      client = Fulfil::Client.new
+      response = client.find_one(model: 'sale.sale', id: 213_112)
+
+      assert_equal 213_112, response['id']
+    end
+
+    def test_find_many
+      stub_fulfil_put('sale.sale/read', 'sale_sale_read', '[[213112,213114],null]')
+
+      client = Fulfil::Client.new
+      orders = client.find_many(model: 'sale.sale', ids: [213_112, 213_114])
+
+      assert_equal 2, orders.count
+      assert_equal 213_112, orders.first['id']
+      assert_equal 213_114, orders.last['id']
+    end
+
+    def test_count
+      stub_fulfil_put('sale.sale/search_count', 'sale_sale_count', '[[]]')
+
+      client = Fulfil::Client.new
+      order_count = client.count(model: 'sale.sale', domain: [])
+
+      assert_equal 362_560, order_count
+    end
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,3 +10,35 @@ require 'minitest/ci' if ENV['CI']
 require 'minitest/autorun'
 
 require 'webmock/minitest'
+
+def load_fixture(name)
+  File.read(File.dirname(__FILE__) + "/fixtures/#{name}.json")
+end
+
+def stub_fulfil_get(path, fixture, status_code = 200)
+  stub_request(:get, "https://fulfil-test.fulfil.io/api/v2/model/#{path}")
+    .with(headers: valid_request_headers)
+    .to_return(status: status_code, body: load_fixture(fixture), headers: valid_response_headers)
+end
+
+def stub_fulfil_put(path, fixture, body, status_code = 200)
+  stub_request(:put, "https://fulfil-test.fulfil.io/api/v2/model/#{path}")
+    .with(headers: valid_request_headers, body: body)
+    .to_return(status: status_code, body: load_fixture(fixture), headers: valid_response_headers)
+end
+
+def valid_request_headers
+  {
+    'Authorization' => 'Bearer user-token',
+    'Connection' => 'Keep-Alive',
+    'Host' => 'fulfil-test.fulfil.io',
+    'User-Agent' => 'http.rb/4.4.1'
+  }
+end
+
+def valid_response_headers
+  {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+  }
+end
