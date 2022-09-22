066031d4ca6964f60b6c6ea424e61a3f0eb2982f
Stefan Vermaas <stefanvermaas@pm.me>
2022-09-22 11:57:36 +0200

    Fix the mocking error after a recent upgrade

--- test/fulfil/model_test.rb
+++ test/fulfil/model_test.rb
@@ -1,14 +1,14 @@
-require 'minitest/autorun'
-require 'minitest/mock'
-require 'fulfil/model'
+# frozen_string_literal: true
+
+require 'test_helper'
 
 module Fulfil
   class ModelTest < Minitest::Test
     def test_query
-      stub = Minitest::Mock.new
-      stub.expect(:search, [], [Hash])
+      stub_request(:put, fulfil_url_for('sale.sale/search_read'))
+        .and_return(status: 200, body: [].to_json, headers: { 'Content-Type': 'application/json'})
 
-      sales = Fulfil::Model.new(client: stub, model_name: 'sale.sale')
+      sales = Fulfil::Model.new(client: Fulfil::Client.new, model_name: 'sale.sale')
       sales.query(id: [1, 2, 3])
 
       assert_equal [['id', 'in', [1, 2, 3]]], sales.query
