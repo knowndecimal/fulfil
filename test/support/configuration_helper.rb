18d0f5f85c8753e6e3d63b5afd93005d78900390
Stefan Vermaas <stefanvermaas@pm.me>
2022-09-22 12:03:17 +0200

    Reset configuration after each test run

--- test/support/configuration_helper.rb
+++ test/support/configuration_helper.rb
@@ -3,8 +3,9 @@
 # Allows temporary setting a configuration option during tests. Switches back
 # to the original configuration after running the test.
 def with_fulfil_config(&block)
-  previous_config = Fulfil.config
-  Fulfil.configure { |config| block.call(config) }
+  Fulfil.configure do |config|
+    block.call(config)
+  end
 ensure
-  Fulfil.config = previous_config
+  Fulfil.config = Fulfil::Configuration.new
 end
