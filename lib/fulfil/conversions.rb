129f611dbca4ecf95071ad246e0136c84114626c
Stephen Swann <swann.stephen@gmail.com>
2023-12-21 11:12:18 -0500

    Create date and datetime conversion to objects

---
+++ lib/fulfil/conversions.rb
@@ -0,0 +1,31 @@
+module Fulfil
+  class Conversions
+    class << self
+      def update_date_and_datetime_fields(domain)
+        domain.each do |params|
+          params.map! do |param|
+            case param
+            when Date
+              date_as_object(param)
+            when DateTime
+              datetime_as_object(param)
+            else
+              param
+            end
+          end
+        end
+      end
+
+      def datetime_as_object(datetime)
+        {
+          __class__: "datetime",
+          iso_string: datetime.new_offset(0).iso8601
+        }
+      end
+
+      def date_as_object(date)
+        datetime_as_object(date.to_datetime)
+      end
+    end
+  end
+end
