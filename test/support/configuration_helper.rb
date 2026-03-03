9b0cd9a02ad7fe9d6586e22b0fce7efb17f79e5e
Zero Vellum <zero@knowndecimal.com>
2026-03-03 16:26:34 -0500

    chore: silence block forwarding cop for ruby 2.7 parser

--- test/support/configuration_helper.rb
+++ test/support/configuration_helper.rb
@@ -2,10 +2,10 @@
 
 # Allows temporary setting a configuration option during tests. Switches back
 # to the original configuration after running the test.
-# rubocop:disable Naming/BlockForwarding, Style/ArgumentsForwarding
+# rubocop:disable Naming/BlockForwarding
 def with_fulfil_config(&block)
   Fulfil.configure(&block)
 ensure
   Fulfil.config = Fulfil::Configuration.new
 end
-# rubocop:enable Naming/BlockForwarding, Style/ArgumentsForwarding
+# rubocop:enable Naming/BlockForwarding
